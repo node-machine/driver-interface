21af3a7fd2dce3e4621956b9084799d06c581b5a
Mike McNeil <mikermcneil@users.noreply.github.com>
2016-02-16 11:53:47 -0600

    Update send-native-query.js

--- machines/send-native-query.js
+++ machines/send-native-query.js
@@ -59,6 +59,8 @@ module.exports = {
     unrecognizedTable: {
       friendlyName: 'Unrecognized table',
       description: 'The provided query failed because it mentions an unrecognized table.',
+      outputVariableName: 'report',
+      outputDescription: 'The `table` property is the name of the unrecognized table.  The `error` property is a JavaScript Error instance containing the raw error from the database.  The `meta` property is reserved for custom adapter-specific extensions.',
       example: {
         table: 'user_accounts',
         error: '===',
