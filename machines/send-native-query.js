c8ffbf1a0028d98312b7e0b5327e6e60469d4cbe
Mike McNeil <mike@balderdash.co>
2016-02-16 14:21:38 -0600

    Remove unrecognizedTable exit (but kept it around as a constant since we'll use it in DDL)

--- machines/send-native-query.js
+++ machines/send-native-query.js
@@ -54,18 +54,6 @@ module.exports = {
         error: '===',
         meta: '==='
       }
-    },
-
-    unrecognizedTable: {
-      friendlyName: 'Unrecognized table',
-      description: 'The provided query failed because it mentions an unrecognized table.',
-      outputVariableName: 'report',
-      outputDescription: 'The `table` property is the name of the unrecognized table.  The `error` property is a JavaScript Error instance containing the raw error from the database.  The `meta` property is reserved for custom adapter-specific extensions.',
-      example: {
-        table: 'user_accounts',
-        error: '===',
-        meta: '==='
-      }
     }
 
   }
