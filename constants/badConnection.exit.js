4efa94ee9909a16af1bf72ecd747eb7407552e35
Mike McNeil <mike@balderdash.co>
2016-02-16 19:26:57 -0600

    Remove 'error' property from badConnection exit.

--- constants/badConnection.exit.js
+++ constants/badConnection.exit.js
@@ -7,9 +7,8 @@ module.exports = {
   description: 'The provided connection is no longer active; or possibly never was.',
   extendedDescription: 'Usually, this means the connection to the database was lost due to a logic error or timing issue in userland code.  In production, this can mean that the database became overwhelemed or was shut off while some business logic was in progress.',
   outputVariableName: 'report',
-  outputDescription: 'The `error` property is a JavaScript Error instance containing the raw error from the database.  The `meta` property is reserved for custom adapter-specific extensions.',
+  outputDescription: 'The `meta` property is reserved for custom adapter-specific extensions.',
   example: {
-    error: '===',
     meta: '==='
   }
 };
