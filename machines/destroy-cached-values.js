20dc2d82334909c8deb61790ec23d2daadceef4b
Mike McNeil <mike@balderdash.co>
2016-04-01 19:11:46 -0500

    Updated destroyCachedValues spec re: https://github.com/mikermcneil/machinepack-redis/commit/398fad027355d7cb21f6dd3a6e674e3bbd9cfc07 and https://github.com/mikermcneil/machinepack-redis/commit/e3449bd74840cb4bbe4daaed1702f6c720fb3c67

--- machines/destroy-cached-values.js
+++ machines/destroy-cached-values.js
@@ -20,7 +20,9 @@ module.exports = {
     keys: {
       friendlyName: 'Keys',
       description: 'An array of unique keys to delete.',
-      extendedDescription: 'The case-sensitivity and allowable characters in keys may vary between drivers.',
+      extendedDescription: 'The case-sensitivity and allowable characters in keys may vary between drivers. '+
+      'If keys are specified more than once, the duplicates will be ignored. And if the array of keys '+
+      'is empty, then no keys will be deleted-- but this method will still exit successfully.',
       required: true,
       example: ['myNamespace.foo.bar_baz']
     },
@@ -38,9 +40,30 @@ module.exports = {
   exits: {
 
     success: {
+      description: 'The specified array of keys were deleted.',
       outputVariableName: 'report',
       outputDescription: 'The `meta` property is reserved for custom driver-specific extensions.',
       example: {
+        meta: '==='
+      }
+    },
+
+    invalidKeys: {
+      description: 'The specified array of keys contains one or more keys which are not valid for this cache.',
+      extendedDescription: 'For example, the driver might reject certain reserved keys.  Or the underlying database might not permit certain keys to be used.',
+      outputVariableName: 'report',
+      outputDescription: 'The `meta` property is reserved for custom driver-specific extensions.',
+      example: {
+        meta: '==='
+      }
+    },
+
+    failed: {
+      description: 'The cache encountered an error while attempting to destroy one or more of the specified keys.',
+      outputVariableName: 'report',
+      outputDescription: 'The `error` property is a JavaScript Error instance explaining the exact error.  The `meta` property is reserved for custom driver-specific extensions.',
+      example: {
+        error: '===',
         meta: '==='
       }
     },
