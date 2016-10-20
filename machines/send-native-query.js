52363203837a2a7577279de1b78058fcb617dd93
Mike McNeil <mikermcneil@users.noreply.github.com>
2016-10-20 16:29:37 -0500

    Make send-native-query expect example: '==='

--- machines/send-native-query.js
+++ machines/send-native-query.js
@@ -14,12 +14,11 @@ module.exports = {
 
     nativeQuery: {
       description: 'A native query for the database.',
-      extendedDescription: 'The provided native query will be coerced to a JSON-serializable value if it isn\'t one already (see [rttc.dehydrate()](https://github.com/node-machine/rttc#dehydratevalue-allownullfalse-dontstringifyfunctionsfalse)). That means any provided Date instances will be converted to timezone-agnostic ISO timestamp strings (i.e. JSON timestamps).',
       whereToGet: {
         description: 'Write a native query for this database, or if this driver supports it, use `compileStatement()` to build a native query from Waterline syntax.',
         extendedDescription: 'This is oftentimes compiled from Waterline query syntax using "Compile statement", however it could also originate from userland code.'
       },
-      example: '*',
+      example: '===',
       required: true
     },
 
