397a0a999085f2e66a710c7aebcebbebde197a1a
Mike McNeil <git+mikermcneil@sailsjs.com>
2018-11-19 01:11:30 -0600

    Add moreInfoURL for s3q input

--- constants/query.input.js
+++ constants/query.input.js
@@ -8,6 +8,7 @@ module.exports = {
   friendlyName: 'Query (s3q)',
   description: 'A stage three Waterline query.',
   extendedDescription: 'The `meta` key of this dictionary is reserved for certain special "meta keys" (e.g. flags, signals, etc.) and other custom, adapter-specific extensions.',
+  moreInfoUrl: 'https://github.com/balderdashy/waterline/blob/90d7113a383fba4bbc77ac4dd2e072634cba2681/ARCHITECTURE.md#stage-3-query',
   required: true,
   readOnly: true,
   example: '==='//e.g. `{ method: 'create', using: 'the_table_name', ... }`
